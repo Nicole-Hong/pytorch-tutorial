7d1f3ec34e46fc380af5201cf91795676a1cbfbc
Stacey Svetlichnaya <stacey@wandb.com>
2019-12-24 17:36:27 -0800

    cleaning up initial hyperparams

--- tutorials/02-intermediate/recurrent_neural_network/main.py
+++ tutorials/02-intermediate/recurrent_neural_network/main.py
@@ -8,15 +8,15 @@ import wandb
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 # Hyperparameters
-run_name = "rnn lr 0.0075 b 64 2 l h 64"
+run_name = "rnn example"
 sequence_length = 28
 input_size = 28
 hidden_size = 64
 num_layers = 2
 num_classes = 10
-batch_size = 64
-num_epochs = 5
-learning_rate = 0.0075
+batch_size = 100
+num_epochs = 2
+learning_rate = 0.01
 
 wb_config = {
   "seq_len" : sequence_length,
