f464ddfa80675399874f565c502857ab1d5745ca
Stacey Svetlichnaya <stacey@wandb.com>
2019-12-30 15:24:30 -0800

    birnn model: sweep compatible

--- tutorials/02-intermediate/bidirectional_recurrent_neural_network/main.py
+++ tutorials/02-intermediate/bidirectional_recurrent_neural_network/main.py
@@ -2,20 +2,40 @@ import torch
 import torch.nn as nn
 import torchvision
 import torchvision.transforms as transforms
-
+import wandb
 
 # Device configuration
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-# Hyper-parameters
+# for reproducibility
+torch.manual_seed(23)
+
+# Hyperparameters
+run_name = "birnn e10 sweep"
 sequence_length = 28
 input_size = 28
 hidden_size = 128
 num_layers = 2
 num_classes = 10
 batch_size = 100
-num_epochs = 2
+num_epochs = 10
 learning_rate = 0.003
+
+wb_config = {
+  "seq_len" : sequence_length,
+  "input_size" : input_size,
+  "hidden_size" : hidden_size,
+  "num_layers" : num_layers,
+  "num_classes" : num_classes,
+  "batch_size" : batch_size,
+  "epochs" : num_epochs,
+  "learning_rate" : learning_rate
+}
+
+# log hyperparameters and settings to wandb
+wandb.init(name=run_name, project="pytorch_intro", config=wb_config)
+# store reference to config to simplify sweeps
+cfg = wandb.config
 
 # MNIST dataset
 train_dataset = torchvision.datasets.MNIST(root='../../data/',
@@ -29,11 +49,11 @@ test_dataset = torchvision.datasets.MNIST(root='../../data/',
 
 # Data loader
 train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
-                                           batch_size=batch_size, 
+                                           batch_size=cfg.batch_size, 
                                            shuffle=True)
 
 test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
-                                          batch_size=batch_size, 
+                                          batch_size=cfg.batch_size, 
                                           shuffle=False)
 
 # Bidirectional recurrent neural network (many-to-one)
@@ -57,8 +77,8 @@ class BiRNN(nn.Module):
         out = self.fc(out[:, -1, :])
         return out
 
-model = BiRNN(input_size, hidden_size, num_layers, num_classes).to(device)
-
+model = BiRNN(input_size, cfg.hidden_size, cfg.num_layers, num_classes).to(device)
+wandb.watch(model, log="all")
 
 # Loss and optimizer
 criterion = nn.CrossEntropyLoss()
@@ -79,24 +99,27 @@ for epoch in range(num_epochs):
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-        
+
+        # log loss to wandb
+        wandb.log({"loss" : loss}) 
         if (i+1) % 100 == 0:
             print ('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}' 
                    .format(epoch+1, num_epochs, i+1, total_step, loss.item()))
 
-# Test the model
-with torch.no_grad():
-    correct = 0
-    total = 0
-    for images, labels in test_loader:
-        images = images.reshape(-1, sequence_length, input_size).to(device)
-        labels = labels.to(device)
-        outputs = model(images)
-        _, predicted = torch.max(outputs.data, 1)
-        total += labels.size(0)
-        correct += (predicted == labels).sum().item()
-
-    print('Test Accuracy of the model on the 10000 test images: {} %'.format(100 * correct / total)) 
+    # Test the model after every epoch
+    with torch.no_grad():
+        correct = 0
+        total = 0
+        for images, labels in test_loader:
+            images = images.reshape(-1, sequence_length, input_size).to(device)
+            labels = labels.to(device)
+            outputs = model(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+        acc = 100 * correct / total
+        wandb.log({"epoch" : epoch, "acc" : acc})
+        print('Test Accuracy of the model on the 10000 test images: {} %'.format(100 * correct / total)) 
 
 # Save the model checkpoint
 torch.save(model.state_dict(), 'model.ckpt')
